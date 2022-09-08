8ddf06fb7547bc04ddbd06f06a07e5bd240016b6
amtrindade <amtrindade@gmail.com>
2022-09-08 19:53:06 -0300

    Adiciona calculadora

--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -5,32 +5,76 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CalculadoraTest {
-	public WebDriver driver;
+	private WebDriver driver;
+	private WebElement tfOne;
+	private WebElement tfTwo;
+	private WebElement tfTotal;
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver",
-				"/home/antonio/dev/drivers/chromedriver"); 
-		//C:\\driver\\chromedriver.exe");
-		driver = new ChromeDriver();		
-		driver.get("http://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");		
+		System.setProperty("webdriver.chrome.driver", "/home/antonio/dev/drivers/chromedriver");
+		// C:\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
+
+		tfOne = driver.findElement(By.id("number1"));
+		tfTwo = driver.findElement(By.id("number2"));
+		tfTotal = driver.findElement(By.id("total"));
 
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
-		driver.quit();	
+		driver.quit();
 	}
 
 	@Test
-	public void testSoma() {
-		
+	public void testSoma() throws InterruptedException {
+		// mapeia os componentes
+		WebElement btnSum = driver.findElement(By.id("somar"));
+
+		// interage com os valores
+		tfOne.sendKeys("4");
+		tfTwo.sendKeys("5");
+		btnSum.click();
+
+		Thread.sleep(3000);
+
+		// valida o resultado
+		assertEquals("9", tfTotal.getAttribute("value"));
 	}
-	
-	
+
+	@Test
+	public void testSubtracao() {
+		// mapeia os componentes
+		WebElement btnMinus = driver.findElement(By.id("subtrair"));
+
+		// interage com os valores
+		tfOne.sendKeys("10");
+		tfTwo.sendKeys("4");
+		btnMinus.click();
+
+		// valida o resultado
+		assertEquals("6", tfTotal.getAttribute("value"));
+	}
+
+	@Test
+	public void testMultiplica() {
+		WebElement btnMultiplicacao = driver.findElement(By.id("multiplicar"));
+
+		// interage com os valores
+		tfOne.sendKeys("10");
+		tfTwo.sendKeys("4");
+		btnMultiplicacao.click();
+
+		// valida o resultado
+		assertEquals("40", tfTotal.getAttribute("value"));
+	}
 
 }
