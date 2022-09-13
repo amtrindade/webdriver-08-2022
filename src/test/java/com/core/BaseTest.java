4a06b932655096ce99f9786d407ff97e3e8d855e
amtrindade <amtrindade@gmail.com>
2022-09-13 20:01:41 -0300

    Cria driverfactory

---
+++ src/test/java/com/core/BaseTest.java
@@ -0,0 +1,11 @@
+package com.core;
+
+import org.junit.After;
+
+public abstract class BaseTest {
+	
+	@After
+	public void tearDown() {
+		DriverFactory.killDriver();
+	}
+}
--- src/test/java/com/test/DragAndDropExecTest.java
+++ src/test/java/com/test/DragAndDropExecTest.java
@@ -1,46 +1,38 @@
 package com.test;
 
+import static com.core.DriverFactory.getDriver;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DragAndDropExecTest {
-	public WebDriver driver;
+import com.core.BaseTest;
 
+public class DragAndDropExecTest extends BaseTest {
+	
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver",
-				"/home/antonio/dev/drivers/chromedriver"); 		
-		driver = new ChromeDriver();			
-		driver.get("http://sahitest.com/demo/dragDropMooTools.htm");		
+		getDriver().get("http://sahitest.com/demo/dragDropMooTools.htm");		
 	}
 	
-	@After
-	public void tearDown() throws Exception {
-		driver.quit();	
-	}
 	
 	@Test
 	public void testValidateItems() {
-		WebElement source = driver.findElement(By.id("dragger"));
+		WebElement source = getDriver().findElement(By.id("dragger"));
 		
-		WebElement item1 = driver.findElement(By.cssSelector("body > div:nth-child(4)"));
-		WebElement item4 = driver.findElement(By.cssSelector("body > div:nth-child(7)"));
+		WebElement item1 = getDriver().findElement(By.xpath("//*[.='Item 1']"));
+		WebElement item4 = getDriver().findElement(By.xpath("//*[.='Item 4']"));
 		
 		assertEquals("Item 1", item1.getText());
 		assertEquals("Item 4", item4.getText());
 		
-		new Actions(driver).dragAndDrop(source, item1).perform();
+		new Actions(getDriver()).dragAndDrop(source, item1).perform();
 		assertEquals("dropped", item1.getText());
 		
-		new Actions(driver).dragAndDrop(source, item4).perform();
+		new Actions(getDriver()).dragAndDrop(source, item4).perform();
 		assertEquals("dropped", item4.getText());
 	}
 
